f8204b2cbc00468257161c8091228b14c3879962
jdm79 <james.malvern@gmail.com>
2021-08-08 18:34:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import  {beers} from './beers';
+
 
 function App() {
   return (
---
+++ src/beers.js
@@ -0,0 +1,38 @@
+export const beers = [
+
+{
+    "id": "1",
+    "beer": "IPA",
+    "company": "Green's",
+    "url": "www.glutenfreebeer.com",
+    "description": "Hoppy magic",
+    "img": ""
+  },
+{
+    "id": "2",
+    "beer": "",
+    "company": "Suspect Brewing",
+    "url": "www.suspectbrewing.com",
+    "description": "Hoppy magic",
+    "img": ""
+  },
+{
+    "id": "3",
+    "beer": "IPA",
+    "company": "Mongozo",
+    "url": "www.mongozo.com",
+    "description": "Hoppy magic",
+    "img": ""
+  },
+{
+    "id": "4",
+    "beer": "IPA",
+    "company": "Stirone",
+    "url": "www.glutenfreebeer.com",
+    "description": "Hoppy magic",
+    "img": ""
+  },
+
+];
+
+
