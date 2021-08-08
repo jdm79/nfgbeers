30886cd0ad6e00d438c52d08691b20367f57c4de
jdm79 <james.malvern@gmail.com>
2021-08-08 19:49:46 +0200

    clean up holder text, add email link

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+JAMES: 
+$  npm run build
+$  netlify deploy --prod 
+
+then choose ./build
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -12,19 +12,10 @@ function App() {
           hello world. more content coming shortly...
         </p>
         <p>
-          this website will soon be listing naturally gluten-free beer available in Europe (and probably everywhere else too in time).
+          this website will list naturally gluten-free beer available in Europe (and probably everywhere else too, in time).
         </p>
         <p>
-          naturally gluten-free beer is beer made without gluten.
-        </p>
-        <p>
-          (i.e not made with barley and then 'deglutenised').
-        </p>
-        <p>
-          we're talking beer for sensitive celiac/gf beer lovers like me. 
-        </p> 
-        <p>
-          please send info on any naturally gluten-free beers to: james.malvern@gmail.com
+          please send info on naturally gluten-free beers available in Europe to: <span><a href="mailto:james.malvern@gmail.com">james.malvern@gmail.com</a></span>
         </p>
       </header>
     </div>
